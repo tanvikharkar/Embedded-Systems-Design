b613705a398c07e98d98b97ee3f0299f8e65beb5
Tanvi Kharkar <tanviykharkar@gmail.com>
2023-04-10 16:21:51 -0700

    Add files via upload

---
+++ assignment-6-tanvikharkar/set_DCO.h
@@ -0,0 +1,14 @@
+#ifndef SET_DCO_H_
+#define SET_DCO_H_
+
+#define FREQ_1_5_MHz CS_CTL0_DCORSEL_0
+#define FREQ_3_MHz CS_CTL0_DCORSEL_1
+#define FREQ_6_MHz CS_CTL0_DCORSEL_2
+#define FREQ_12_MHz CS_CTL0_DCORSEL_3
+#define FREQ_24_MHz CS_CTL0_DCORSEL_4
+#define FREQ_48_MHz CS_CTL0_DCORSEL_5
+
+// function prototype
+void set_DCO(uint32_t frequency);
+
+#endif /* SET_DCO_H_ */
