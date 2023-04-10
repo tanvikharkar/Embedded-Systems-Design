67755ec67820d056bd4fad9183bef5feb4158579
Tanvi Kharkar <tanviykharkar@gmail.com>
2023-04-10 11:41:15 -0700

    Add files via upload

--- assignment-1-tanvikharkar/main.c
+++ assignment-1-tanvikharkar/main.c
@@ -59,9 +59,11 @@ num = (num ^ (0xFF)); // XOR w/ 1 toggles all bits
 }
 return num;
 }
+
 /**
 * main.c
 */
+
 void main(void)
 {
 WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD; // stop watchdog timer
